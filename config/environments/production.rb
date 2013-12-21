f9f9c9dd28ac7d4cc682a0b7ef218504cf752fb5
John <kohler.j@husky.neu.edut>
2013-12-21 17:11:37 -0500

    production.rb change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-MusicBlog::Application.configure do
+FiveSomewhere::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
